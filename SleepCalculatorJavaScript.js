475fed4e40500ed2322925c3d337e91f882d6ec9
Payden Dyer <paydendyer@Paydens-MacBook-Air.local>
2020-04-28 17:14:36 -0500

    .

---
+++ SleepCalculatorJavaScript.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+    // This is the length of a sleep cycle, expressed in milliseconds
+    //var cycle = 90 * 60000;
+    //*Calculate times user should go to sleep if trying to wake up by a specific time
+       
+       let calcSleepTimes = (minute,hour) => {
+         if 
+        var timesList = [];
+        for (var j = 2; j < 7; j++) {
+          var d = new Date(dateTime + j * 90 * 60000);
+          timesList.push(d);
+        }
+        return timesList;
+      },
+    };
